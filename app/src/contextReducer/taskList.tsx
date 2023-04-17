7d9aacf674de26c8104a1fe0edaab6f57f75d638
maxgerashenko <mgerashchenko@gmail.com>
2023-04-17 17:38:52 -0400

    1 file modified

--- app/src/contextReducer/taskList.tsx
+++ app/src/contextReducer/taskList.tsx
@@ -1,30 +1,70 @@
 import { useState, useContext } from 'react';
-import { useTasksDispatch } from '../contextReducer/contexts';
+import { useTasks, useTasksDispatch } from './contexts';
 
-export default function AddTask() {
-  const [text, setText] = useState('');
-  const dispatch: any = useTasksDispatch();
+export default function TaskList() {
+  const tasks: any = useTasks();
   return (
-    <>
-      <input
-        placeholder="Add task"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          setText('');
-          dispatch({
-            type: 'added',
-            id: nextId++,
-            text: text,
-          });
-        }}
-      >
-        Add
-      </button>
-    </>
+    <ul>
+      {tasks.map((task: any) => (
+        <li key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
-let nextId = 3;
+function Task({ task }: any) {
+  const [isEditing, setIsEditing] = useState(false);
+  const dispatch: any = useTasksDispatch();
+  const taskContent = isEditing ? (
+    <>
+      <input
+        value={task.text}
+        onChange={(e) => {
+          dispatch({
+            type: 'changed',
+            task: {
+              ...task,
+              text: e.target.value,
+            },
+          });
+        }}
+      />
+      <button onClick={() => setIsEditing(false)}>Save</button>
+    </>
+  ) : (
+    <>
+      {task.text}
+      <button onClick={() => setIsEditing(true)}>Edit</button>
+    </>
+  );
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={(e) => {
+          dispatch({
+            type: 'changed',
+            task: {
+              ...task,
+              done: e.target.checked,
+            },
+          });
+        }}
+      />
+      {taskContent}
+      <button
+        onClick={() => {
+          dispatch({
+            type: 'deleted',
+            id: task.id,
+          });
+        }}
+      >
+        Delete
+      </button>
+    </label>
+  );
+}
