6db059d1916b15263cf5bf1b64973f6990f2b92a
maxgerashenko <mgerashchenko@gmail.com>
2023-05-10 23:00:46 -0400

    2 files modified

--- app/src/themeProvider.tsx
+++ app/src/themeProvider.tsx
@@ -1,8 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeContext } from './contexts';
 
+function useOnThemeChange(setTheme: (value: string) => void) {
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  useEffect(() => {
+    function onThemeChange(isDarkMode: boolean) {
+      setTheme(isDarkMode ? 'dark' : 'light');
+    }
+    const isDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    onThemeChange(isDarkMode);
+
+    const handler = (e: MediaQueryListEventInit) => {
+      onThemeChange(!!e.matches);
+    };
+
+    mediaQuery.addEventListener('change', handler);
+    return () => mediaQuery.removeEventListener('change', handler);
+  }, []);
+}
+
 export function ThemeProvider({ children }: any) {
-  const [theme, setTheme]: any = useState<string>('light');
+  const [theme, setTheme]: any = useState<string>('dark');
+
+  useOnThemeChange(setTheme);
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
