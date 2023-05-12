0c3cad0ca889c50cc0a6400871d284ccf7131197
maxgerashenko <mgerashchenko@gmail.com>
2023-05-12 03:03:46 -0400

    2 files modified

--- app/src/course/propsToList.tsx
+++ app/src/course/propsToList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 interface Article {
   title: string;
@@ -32,21 +32,21 @@ function List(props: { list: Article[] }) {
 export function PropsToList() {
   const [stories, setStories] = useState([
     {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
-      objectID: 0
+      objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
-      objectID: 1
-    }
+      objectID: 1,
+    },
   ] as Article[]);
   return (
     <div>
